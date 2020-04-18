901732c6f35588508501bf64ab92407fdedb94bb
Wei-Hong Tseng <w.h.tseng@weihongemacbook.home>
2020-04-18 15:29:41 +0100

    Updated

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd /Users/tsengweihong/Documents/mrtseng
+cd ”/Users/w.h.tseng/Library/Mobile\ Documents/com~apple~CloudDocs/Documents/mrtseng“
 
 git add *;
 git commit -m "Updated";
